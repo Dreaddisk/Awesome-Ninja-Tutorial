5571aa8a7fa7c17d9ac490d03836764398d64e39
Jose <joey_manuel@outlook.com>
2021-01-23 08:44:47 -0500

    11. Skills(2)
    Created the following scripts:
    -Skill_Move.cs
    - Skill_Effect.cs

---
+++ Awesome-Ninja-Game/Assets/Scripts/Skills Scripts/Skill_Effect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_Effect : MonoBehaviour
+{
+
+    #region Variables
+    public LayerMask ignoreLayers;
+    public GameObject skillEffectPrefab;
+
+    public float radius;
+
+    private bool collided = false;
+    #endregion Variables
+
+
+    #region UnityFunctions
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, ~ignoreLayers);
+
+        foreach(Collider c in hits)
+        {
+            if(c.isTrigger)
+            {
+                continue;
+            }
+            collided = true;
+        }
+
+        if(collided)
+        {
+            Instantiate(skillEffectPrefab, transform.position, transform.rotation);
+            DestroyObject(gameObject);
+        }
+    }
+
+    #endregion UnityFunctions
+}
---
+++ Awesome-Ninja-Game/Assets/Scripts/Skills Scripts/Skill_Move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Skill_Move : MonoBehaviour
+{
+
+    #region Variables
+    public float X, Y, Z = 0;
+
+    public bool local = false;
+    #endregion Variables
+
+
+    #region UnityFunctions
+
+    private void Update()
+    {
+        if(local)
+        {
+            transform.Translate(new Vector3(X, Y, Z) * Time.deltaTime, Space.World);
+        }
+    }
+    #endregion UnityFunctions
+}
