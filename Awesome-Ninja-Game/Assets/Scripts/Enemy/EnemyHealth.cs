89aee6ecc1daa179d33bfb082b08fbdaed42d831
Jose <joey_manuel@outlook.com>
2021-01-23 09:51:57 -0500

    12. Player and EnemyHealth
    created the EnemyHealth and PlayerHealth scripts.

---
+++ Awesome-Ninja-Game/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    #region Varaiables
+    public float realHealth;
+
+    private AudioSource audioSource;
+    private AudioClip enemyDeadSound;
+
+    private bool enemyDead;
+    private bool enemyIsHit;
+
+    private Animator anim;
+
+    public GameObject deadEffect;
+    public Transform deadEffectPoint;
+
+    public GameObject attackPointOne;
+    public GameObject attackPointTwo;
+
+    // Animation States
+    private string ANIMATION_ATTACK = "Attack";
+    private string ANIMATION_BE_ATTACKED = "BeAttacked";
+    private string ANIMATION_DEAD = "Dead";
+
+    private string BASE_LAYER_DYING = "Base Layer.Dying";
+    private string BASE_LAYER_HIT = "Base Layer.Hit";
+
+    #endregion Variables
+
+
+    #region UnityFunctions
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyDead)
+        {
+            
+        }
+    }
+
+
+
+    #endregion
+
+    void EnemyDying()
+    {
+        anim.SetBool(ANIMATION_DEAD, true);
+        anim.SetBool(ANIMATION_BE_ATTACKED, false);
+        enemyDead = true;
+        StartCoroutine(DeadEffect());
+        attackPointOne.SetActive(false);
+        attackPointTwo.SetActive(false);
+        audioSource.PlayOneShot(enemyDeadSound);
+    }
+
+    void StopDeadAnimation()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(BASE_LAYER_DYING))
+        {
+            anim.SetBool(ANIMATION_DEAD, false);
+        }
+    }
+
+    public void EnemyTakeDamage(float amount)
+    {
+        realHealth -= amount;
+
+        if (realHealth <= 0)
+        {
+            realHealth = 0;
+            EnemyDying();
+        }
+
+        if (amount > 0)
+        {
+            enemyIsHit = true;
+        }
+    }
+
+    void EnemyAttacked()
+    {
+        enemyIsHit = false;
+        anim.SetBool(ANIMATION_BE_ATTACKED, true);
+        anim.SetBool(ANIMATION_ATTACK, false);
+        transform.Translate(Vector3.back * 0.5f);
+    }
+
+    void StopEnemyHit()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(BASE_LAYER_HIT))
+        {
+            anim.SetBool(ANIMATION_BE_ATTACKED, false);
+        }
+    }
+
+    IEnumerator DeadEffect()
+    {
+        yield return new WaitForSeconds(2f);
+        Instantiate(deadEffect, deadEffectPoint.position, deadEffectPoint.rotation);
+        Destroy(gameObject);
+    }
+
+} // enemyHealth class
---
+++ Awesome-Ninja-Game/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    #region Variables
+    public float realHealth;
+
+    private Animator anim;
+    private bool playerDead;
+    private bool playerBeHit;
+
+    private Slider healthSlider;
+    private Text healthText;
+
+    // private BossHealth bossHealth;
+    private Transform bossTransform;
+    private bool victory;
+
+    private string ANIMATION_DEAD = "Dead";
+    private string ANIMATION_ATTACK = "Attack";
+    private string ANIMATION_VICTORY = "Victory";
+
+    private string BASE_LAYER_DYING = "Base Layer.Dying";
+    private string BASE_LAYER_VICTORY = "Base Layer.Victory";
+    #endregion
+
+
+    #region UnityFunctions
+    // Use this for initialization
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        healthSlider = GameObject.Find("Health Foregrouund").GetComponent<Slider>();
+        healthText = GameObject.Find("Health Text").GetComponent<Text>();
+
+        healthText.text = realHealth.ToString();
+        healthSlider.value = realHealth;
+
+        bossTransform = GameObject.FindGameObjectWithTag("Boss").transform;
+       // bossHealth = bossTransform.gameObject.GetComponent<BossHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (realHealth <= 0)
+        {
+            realHealth = 0;
+            PlayerDying();
+        }
+
+        if (playerDead)
+        {
+            StopPlayerDeadAnimation();
+        }
+
+            if (bossHealth.realHealth <= 0) {
+			Victory();
+		}
+
+		if (victory) {
+			StopVictoryAnimation();
+		}
+
+    */
+    }
+
+    #endregion UnityFunctions
+
+    void PlayerDying()
+    {
+        playerDead = true;
+        anim.SetBool(ANIMATION_DEAD, true);
+        anim.SetBool(ANIMATION_ATTACK, false);
+    }
+
+    void StopPlayerDeadAnimation()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(BASE_LAYER_DYING))
+        {
+            anim.SetBool(ANIMATION_DEAD, false);
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        realHealth -= amount;
+
+        if (realHealth <= 0)
+        {
+            realHealth = 0;
+        }
+
+        if (amount > 0)
+        {
+            healthText.text = realHealth.ToString();
+            healthSlider.value = realHealth;
+            playerBeHit = true;
+        }
+    }
+
+    void Victory()
+    {
+        anim.SetBool(ANIMATION_VICTORY, true);
+        victory = true;
+    }
+
+    void StopVictoryAnimation()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(BASE_LAYER_VICTORY))
+        {
+            anim.SetBool(ANIMATION_VICTORY, false);
+        }
+    }
+
+
+} // PlayerHealth class
