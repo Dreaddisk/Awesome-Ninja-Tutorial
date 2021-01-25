63ab78968f1cd1ae43f761fc2093c3fd3d9aaeaa
Jose <joey_manuel@outlook.com>
2021-01-25 18:29:53 -0500

    15. Player Commands
    The following scripts were created:
    - PlayerAttackbutton.cs
    - WaitForAttackAgain.cs
    - PlayerMoveJoystick.cs

---
+++ Awesome-Ninja-Game/Assets/Scripts/Player Scripts/Attack Scripts/PlayerAttackButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackButton : MonoBehaviour
+{
+    private PlayersAttacks playerAttack;
+
+    private void Awake()
+    {
+        playerAttack = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayersAttacks>();
+    }
+
+    public void OnPointerDown()
+    {
+        if(gameObject.name == "Attack Button")
+        {
+            playerAttack.AttackButtonPressed();
+        }
+    }
+
+    public void OnPointerUp()
+    {
+        if (gameObject.name == "Attack Button")
+        {
+            playerAttack.AttackButtonReleased();
+        }
+    }
+}
